1df27b9ea3f37b34cf6d84a6ab0bfe9aba78501e
trakuta <timur.rakuta@gmail.com>
2022-12-11 17:19:44 +0300

    l_test

--- llst.py
+++ llst.py
@@ -90,10 +90,13 @@ class LinkedList:
         for i in range(len):
             self.push_front(random.randint(0, 100))
 
+
 def main():
     l_test = LinkedList()
     l_test.generate(5)
     l_test.show()
 
+
 if __name__ == '__main__':
     main()
+    
